8fe966fbea32077a08d623be7031b524813de095
Paul Miller <johnparkinson142@gmail.com>
2015-04-16 11:05:39 +0100

    added tests

---
+++ tests/hrgo.js
@@ -0,0 +1,84 @@
+module.exports = {
+  'google': function (client) {
+    client
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'hrgo')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('ol#rso', 'HRGO');
+  },
+  '404' : function (client) {
+    client
+      .maximizeWindow()
+      .url('http://hrgo.co.uk/thiswillbea404')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.title('404', '404 page');
+  },
+  'home' : function (client) {
+    client
+      .url('http://hrgo.co.uk/')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.urlEquals('http://www.hrgo.co.uk/', 'We are redirected to http://www.hrgo.co.uk/')
+      .verify.elementPresent('.home-hero', 'Home Hero section is displayed')
+      .verify.elementPresent('.navigation', 'Navbar macro is working')
+      .verify.elementPresent('a[href^="/about/"]', 'Navbar links are rendered')
+      .verify.elementPresent('.subNavigation', 'Subnav macro is working')
+      .verify.hidden('.subNavigation', 'Subnav is not currently visible')
+      .moveToElement('a[href^="/locations/"]', 10, 10)
+      .verify.visible('.subNavigation', 'Subnav is visible when dropdown is hovered')
+      .verify.elementPresent('.subNavigation a[href^="/head-office/"]', 'SubNavigation links are rendered')
+      .verify.elementPresent('.navigation', 'Footer macro is working')
+      .click('.navigation a[href^="/locations/"]')
+      .pause(500)
+      .verify.urlEquals('http://www.hrgo.co.uk/locations/', 'we navigated to locations')
+      .click('.navigation .logo')
+      .pause(500)
+      .verify.urlEquals('http://www.hrgo.co.uk/', 'Clicking the logo brought us to home page again');
+
+  },
+  'about' : function (client){
+    client
+      .url('http://hrgo.co.uk/about')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.elementPresent('.clients-hero', 'About page has a hero section')
+      .verify.elementPresent('.person-card-narrow', 'Senior Management team are being displayed');
+  },
+  'locations' : function (client) {
+    client
+      .url('http://hrgo.co.uk/locations')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.elementPresent('.locations-hero', 'Locations page has a hero section')
+      .verify.elementPresent('.branch-card', 'Locations macro is working');
+  },
+  'branch' : function (client) {
+    client
+      .url('http://hrgo.co.uk/locations/dudley')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.containsText('h1', 'Dudley', 'We have reached the dudley branch page')
+      .verify.elementPresent('.industry-specialisms', 'Sector cards have been displayed')
+      .verify.elementPresent('.person-card-wide', 'Members of the team are being displayed');
+  },
+  'sectors' : function (client) {
+    client
+      .url('http://hrgo.co.uk/sectors')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.elementPresent('.industries-hero', 'We have reached the sectors page')
+      .verify.elementPresent('.industry-card-wide', 'Sectors macro is working');
+  },
+  'blog' : function (client) {
+    client
+      .url('http://hrgo.co.uk/blog')
+      .waitForElementPresent('body', 500, 'page loaded')
+      .verify.elementPresent('.blog-hero', 'We have reached the blog page')
+      .verify.elementPresent('.blog-hero h1', 'Featured post is rendered')
+      .verify.elementPresent('.blog-post', 'Blog posts are being displayed');
+  },
+  'end': function (client) {
+    client
+      .end();
+  }
+};
